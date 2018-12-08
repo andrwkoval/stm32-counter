1308dbafb8a42db668c59fd281c491f7e25bcff5
Andrii Koval <drone.kov@gmail.com>
2018-12-08 18:30:23 +0200

    Initial commit

---
+++ Debug/Src/main.d
@@ -0,0 +1,82 @@
+Src/main.o: ../Src/main.c \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/main.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/stm32f4xx_hal_conf.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/main.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cmInstr.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cmFunc.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cmSimd.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/gpio.h
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/main.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/stm32f4xx_hal_conf.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/main.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/Users/andriykoval/Documents/Programming_Cpp/mcu_lab_02/Inc/gpio.h:
